77bc3083608fec3baa0be7ee6959592db5e9547c
Etel Sverdlov <beigeotter@gmail.com>
2013-01-19 15:37:53 -0500

    Bunch of cool shit and jQuery!

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -8,3 +8,13 @@ $('.recommend_activity').live('click', function() {
   $('.what_to_do').hide();
   $('.recommend_me_something').show();
 });
+
+
+$('.click-tag').live('click', function() {
+  if ($(this).is('.selected')) {
+    $(this).removeClass('selected');
+  }
+  else {
+    $(this).addClass('selected');
+  }
+});
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,7 @@
 class PublicController < ApplicationController
 	def index
 		@activities = Activity.all
+		@tags =Tag.all
 	end
 
 
